a26a8a857e83d1d7fa252c9c13a87082fd09c039
dAKdibeam <kriskorn2u@gmail.com>
2019-08-27 00:04:30 +0700

    first commit

---
+++ WebAPI/Models/PaymentDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PMId { get; set; }
+        [Required] 
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwernerName { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber {get;set;}
+        [Required]
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExprirationDate { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(3)")]
+        public string CVV { get; set; }
+        
+    }
+}
